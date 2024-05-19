bd0026c9d61943c567bb458289012997a225f4cb
Mirjam <mirjam.weibel@outlook.com>
2024-05-19 13:02:35 +0200

    websocket apassige

--- src/main/java/ch/uzh/ifi/hase/soprafs24/WebSocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/WebSocketController.java
@@ -23,6 +23,7 @@ public class WebSocketController {
 
     public final Map<Long, Set<String>> readyPlayers = new ConcurrentHashMap<>();
     public final Map<Long, Set<String>> connectedPlayers = new ConcurrentHashMap<>();
+    public final Map<Long, Set<String>> submittedPlayers = new ConcurrentHashMap<>();
     private final SimpMessagingTemplate messagingTemplate;
     private final RoundService roundService;
     private final RoundRepository roundRepository;
@@ -102,12 +103,51 @@ public class WebSocketController {
     }
 
 
+    @MessageMapping("/answers-submitted")
+    @SendTo("/topic/answers-count")
+    public String answers(@Payload Map<String, String> payload) {
+        String username = payload.get("username");
+        Long lobbyId;
+
+        try {
+            lobbyId = Long.parseLong(payload.get("lobbyId"));
+        } catch (NumberFormatException e) {
+            System.out.println("Error parsing lobby ID from: " + payload.get("lobbyId") + ", Error: " + e.getMessage());
+            return "{\"error\":\"Invalid lobby ID\"}";
+        }
+
+        Set<String> lobbyConnected = connectedPlayers.computeIfAbsent(lobbyId, k -> ConcurrentHashMap.newKeySet());
+        Set<String> answersSubmitted = submittedPlayers.computeIfAbsent(lobbyId, k -> ConcurrentHashMap.newKeySet());
+
+        if (!lobbyConnected.contains(username)) {
+            System.out.println("User " + username + " not found in lobby " + lobbyId);
+            return "{\"error\":\"User " + username + " not found in lobby " + lobbyId + "\"}";
+        }
+
+        answersSubmitted.add(username);
+        if (checkallAnswers(lobbyId)) {
+            submittedPlayers.remove(lobbyId);
+            return String.format("{\"command\":\"done\", \"lobbyId\":%d}", lobbyId);
+        } else {
+            return String.format("{\"error\":\"Not all connected players are ready\", \"lobbyId\":%d}", lobbyId);
+        }
+    }
+
+    private Boolean checkallAnswers(Long lobbyId) {
+        Set<String> lobbyConnected = connectedPlayers.getOrDefault(lobbyId, Collections.emptySet());
+        Set<String> answersSubmitted = submittedPlayers.getOrDefault(lobbyId, Collections.emptySet());
+        // Start the game if all connected players are ready, or if there is only one player who is ready.
+        return lobbyConnected.equals(answersSubmitted) || (lobbyConnected.size() == 1 && answersSubmitted.size() == 1);
+    }
+
+
     private Boolean checkAndStartGame(Long lobbyId) {
         Set<String> lobbyConnected = connectedPlayers.getOrDefault(lobbyId, Collections.emptySet());
         Set<String> lobbyReady = readyPlayers.getOrDefault(lobbyId, Collections.emptySet());
         // Start the game if all connected players are ready, or if there is only one player who is ready.
         return lobbyConnected.equals(lobbyReady) || (lobbyConnected.size() == 1 && lobbyReady.size() == 1);
     }
+
 
     private String sendOnlineAndReadyCount(Long lobbyId) {
         int onlineCount = connectedPlayers.getOrDefault(lobbyId, Collections.emptySet()).size();
@@ -172,7 +212,8 @@ public class WebSocketController {
     }
 
     @MessageMapping("/submit-votes")
-    public void handleVoteSubmission(@Payload Map<String, Object> payload) throws Exception {
+    @SendTo("/topic/final-scores")
+    public String handleVoteSubmission(@Payload Map<String, Object> payload) throws Exception {
         Long lobbyId = Long.valueOf(payload.get("lobbyId").toString());
         String username = payload.get("username").toString();
         Optional<Lobby> optionalLobby = lobbyRepository.findById(lobbyId);
@@ -190,11 +231,13 @@ public class WebSocketController {
         // Check if all players have submitted their votes
         if (roundService.areAllVotesSubmitted(gameId)) {
             // All votes submitted, proceed to calculate final scores
-            Map<String, Map<String, Map<String, Object>>> finalScores = roundService.calculateFinalScores(lobbyId);
-            messagingTemplate.convertAndSend("/topic/final-scores", finalScores);
+            // Map<String, Map<String, Map<String, Object>>> finalScores = roundService.calculateFinalScores(lobbyId);
+            // messagingTemplate.convertAndSend("/topic/final-scores", finalScores);
+            return String.format("{\"command\":\"final\", \"lobbyId\":%d}", lobbyId);
         } else {
             // Not all votes are in, send an update to clients
-            messagingTemplate.convertAndSend("/topic/vote-updates", voteUpdates);
+            //messagingTemplate.convertAndSend("/topic/vote-updates", voteUpdates);
+            return String.format("{\"error\":\"Not all connected players have finished voting\", \"lobbyId\":%d}", lobbyId);
         }
     }
 
